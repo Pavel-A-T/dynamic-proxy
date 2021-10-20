405104ec794c295b9865b2f75d9bf60cd7b2a34e
Pavel Titov <otrepiev.grishka@yandex.ru>
2021-10-20 11:17:55 +0300

    use Set

--- src/main/java/MyInvocationHandler.java
+++ src/main/java/MyInvocationHandler.java
@@ -3,9 +3,13 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MyInvocationHandler implements InvocationHandler {
     private final Object target;
+    private static Set box = new HashSet();
 
     public MyInvocationHandler(Object target) {
         this.target = target;
@@ -15,6 +19,13 @@ public class MyInvocationHandler implements InvocationHandler {
         // создание динамического прокси
         InvocationHandler handler = new MyInvocationHandler(target);
         ClassLoader classLoader = target.getClass().getClassLoader();
+        Method[] methods = target.getClass().getDeclaredMethods();
+        for (Method m: methods) {
+            Annotation[] annotations = m.getDeclaredAnnotations();
+            if (Arrays.stream(annotations).anyMatch(e -> e.annotationType().equals(LogAnnotation.class))) {
+                box.add(m);
+            }
+        }
         return Proxy.newProxyInstance(classLoader, classes, handler);
     }
 
@@ -24,17 +35,14 @@ public class MyInvocationHandler implements InvocationHandler {
         //System.out.println(target.getClass().getMethod(method.getName()).getAnnotation(LogAnnotation.class));
         Class<?>[] parameterTypes = method.getParameterTypes();
         Method methodTemplate = this.target.getClass().getMethod(method.getName(), parameterTypes );
-        Annotation[] annotations = methodTemplate.getDeclaredAnnotations();
-        for (Annotation annotation : annotations) {
-            if (annotation.annotationType().equals(LogAnnotation.class)) {
-                String name = method.getName();
-                //Старт метода
-                System.out.println("Method " + name + " starting work." );
-                Object result = run(method, args);
-                //отчитываемся, что отработал метод
-                System.out.println("Method " + name + " finishing work.");
-                return result;
-            }
+        if (box.contains(methodTemplate)) {
+            String name = method.getName();
+            //Старт метода
+            System.out.println("Method " + name + " starting work." );
+            Object result = run(method, args);
+            //отчитываемся, что отработал метод
+            System.out.println("Method " + name + " finishing work.");
+            return result;
         }
         return run(method, args);
     }
